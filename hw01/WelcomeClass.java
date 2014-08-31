f3ecf59ac705390036a396e4fd7a11ec568588ae
gjj215 <gjacobslu@gmail.com>
2014-08-31 18:10:02 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,22 @@
+////////////////////////////
+// Greg Jacobs
+// Homework 1 - CSE 002
+// August 29, 2014
+//
+public class WelcomeClass{
+    public static void main(String[] args){
+        //Print welcome message with my user ID
+        System.out.println("    -----------");
+        System.out.println("    | WELCOME |");
+        System.out.println("    -----------");
+        System.out.println("  ^  ^  ^  ^  ^  ^");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\"); //Use double backslashes to only
+        System.out.println("|-G--J--J--2--1--5-|");      //print one of them.
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+        System.out.println("  v  v  v  v  v  v");
+        //Print out short autobioographic statement
+        System.out.println("I've gone hiking in the Austrian Alps and I plan");
+        System.out.println("to go back to backpack Europe for a summer");
+        
+    }
+}
