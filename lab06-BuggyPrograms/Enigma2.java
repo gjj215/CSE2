56a7fb285341ae780ff6bc31da46d3c703a5d439
gjj215 <gjacobslu@gmail.com>
2014-10-13 00:21:24 +0000

    Updated Engima2

--- lab06-BuggyPrograms/Enigma2.java
+++ lab06-BuggyPrograms/Enigma2.java
@@ -19,6 +19,11 @@ public class Enigma2{
  Exception in thread "main" java.lang.ArithmeticException: / by zero
         at Enigma2.main(Enigma2.java:9)
         
+    An exception is thrown when there is a runtime error in the program. When an ArithmeticException is thrown, the
+    programmer knows that the runtime error is caused by an arithmetic error. The error tells the programmer which
+    method it is in, in this case "main", and it tells the programmer what the error is, in this case
+    "/ by zero". So the runtime error is on line 9 when 40 is divided by 0.
+        
  * 
  * 
  * 
